ee1177d27729b2a6dcb8565b1901c9081e33c200
leorog <leo.rog@gmail.com>
2021-03-25 20:25:49 -0300

    add checkout page and finish client code

--- client/src/main/app.cljs
+++ client/src/main/app.cljs
@@ -8,11 +8,14 @@
 
 (defonce api-url "http://localhost:8080/v1/")
 
+(def initial-state {:games-selected #{}
+                    :current-page   :search
+                    :games-found    []
+                    :last-query     nil
+                    :page-id        1})
+
 (def state
-  (r/atom {:selected #{}
-           :games-found []
-           :last-query nil
-           :page-id 1}))
+  (r/atom initial-state))
 
 (defn search-games
   ([query]
@@ -24,7 +27,21 @@
                                                   {:query-params {"q" query
                                                                   "page" page}}))]
        (when success
-         (reset! state (assoc @state :games-found body))))) ))
+         (swap! state assoc :games-found body)))) ))
+
+(defn rent!
+  [games]
+  (go
+    (let [{:keys [:success :body]} (<! (http/post (str api-url "rent")
+                                                  {:json-params {:game-ids (map :id games)}}))
+          games-by-id (group-by :id games)]
+      (if success
+        (do
+          (js/alert "Order Completed! Thank you")
+          (reset! state initial-state))
+        (do
+          (js/alert (str (get-in games-by-id [(:already-rented body) 0 :name]) " is already rented. It will be removed from your selection.\nTry Again"))
+          (swap! state assoc :games-selected (flatten (vals (dissoc games-by-id (:already-rented body))))))))))
 
 (defn get-next-page
   []
@@ -40,13 +57,15 @@
   []
   [:div.inline-block
    [:table
-    (for [{:keys [:id :name :image]} (:games-found @state)]
-      [:tr
-       [:td [:img {:src (:thumb_url image) :width 200 :height 200}]]
-       [:td name]
-       [:td [:input {:type :button
-                     :value "add to cart"
-                     :on-click #(swap! state update :selected conj id)}]]])]
+    [:tbody
+     (for [{:keys [:id :name :image] :as game} (:games-found @state)]
+       ^{:key id}
+       [:tr
+        [:td [:img {:src (:thumb_url image) :width 200 :height 200}]]
+        [:td name]
+        [:td [:input {:type :button
+                      :value "add to cart"
+                      :on-click #(swap! state update :games-selected conj game)}]]]) ]]
    (when (= 10 (count (:games-found @state)))
      [:div.inline-block
       [:input {:type :button
@@ -57,26 +76,53 @@
                :on-click #(get-next-page)
                :value "next page"}]])])
 
+(defn search-page
+  []
+  [:div
+   [:label "game by name or tag:"]
+   [:input {:on-input #(swap! state assoc :last-query (-> % .-target .-value))}]
+   [:input {:type :button
+            :value "search"
+            :on-click #(search-games (:last-query @state))}]
+   [:div (when (seq (:games-selected @state)) (str "cart: " (map :name (:games-selected @state))))]
+   [:div (when (seq (:games-found @state)) (str "found: " (count (:games-found @state))))]
+
+   [game-list-view]
+   (when (seq (:games-selected @state))
+     [:input {:type :button
+              :on-click #(swap! state assoc :current-page :checkout)
+              :value "CHECKOUT"}])])
+
+(defn checkout-view
+  []
+  [:div
+   [:h4 "Games you choose:"]
+   [:table
+    [:tbody
+     (for [{:keys [:id :name :image]} (:games-selected @state)]
+       ^{:key id}
+       [:tr
+        [:td [:img {:src (:thumb_url image) :width 200 :height 200}]]
+        [:td name]])]]
+   [:input {:type :button
+            :on-click #(rent! (:games-selected @state))
+            :value "RENT"}]
+   (when-not (seq (:games-selected @state))
+     [:input {:type :button
+              :on-click #(reset! state initial-state)
+              :value "GO BACK TO SEARCH"}])])
+
+(defn get-view
+  [current-view]
+  (case current-view
+    :search search-page
+    :checkout checkout-view))
+
 (defn main-component
   []
   [:div
    [:h1 "GAME SHOP"]
-   [:div
-    [:label "game by name or tag:"]
-    [:input {:on-input #(swap! state assoc :last-query (-> % .-target .-value))}]
-    [:input {:type :button
-             :value "search"
-             :on-click #(search-games (:last-query @state))}]
-    [:div (when (seq (:selected @state)) (str "cart: " (:selected @state)))]
-    [:div (when (seq (:games-found @state)) (str "found: " (count (:games-found @state))))]
-
-    [game-list-view]
-    (when (seq (:selected @state))
-      [:input {:type :button
-               :value "CHECKOUT"}])]
-
-   [:div.w-full
-    [:div.m-10 {:id "chart"}]]])
+   [(get-view (:current-page @state))]])
 
 (defn mount
   [c]
